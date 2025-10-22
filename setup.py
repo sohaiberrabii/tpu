48f14496c49ff1b159f332ca1f672e56599c082f
Sohaib Errabii <errabii.sohaib@gmail.com>
2025-10-22 10:53:21 +0200

    missing setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="naccel",
+    version="0.1.0",
+    author="Sohaib Errabii",
+    author_email="sohaib.errabii@inria.fr",
+    description="TPU in amaranth",
+    packages=find_packages(),    
+    python_requires=">=3.10",
+    install_requires=[
+        "amaranth[builtin-yosys] @ git+https://github.com/amaranth-lang/amaranth.git",
+        "amaranth_soc @ git+https://github.com/sohaiberrabii/amaranth-soc@patch-1",
+    ],
+    extras_require={
+        "test": [ 
+            "pytest",
+            "numpy",
+        ],
+        "cocotb": [
+            "cocotb @ git+https://github.com/cocotb/cocotb.git",
+        ],
+        "pynq": [
+            "pynq",
+        ],
+    },
+
+)
