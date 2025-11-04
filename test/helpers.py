70cd0108150cc00153cdef993c6074e80e7d0a34
Sohaib Errabii <errabii.sohaib@gmail.com>
2025-11-04 09:36:44 +0100

    remove amaranth test_tpu for now

--- test/helpers.py
+++ test/helpers.py
@@ -20,66 +20,21 @@ def run_sim(dut, tb, processes=[], comb=False, vcdfn=None):
         with sim.write_vcd(vcdfn):
            sim.run()
 
-async def axi4lite_write(ctx, bus, addr, wdata):
-    ctx.set(bus.bready, 1)
-    ctx.set(bus.awvalid, 1)
-    ctx.set(bus.wvalid, 1)
-    ctx.set(bus.awaddr, addr)
-    ctx.set(bus.wdata, wdata)
-    await ctx.tick()
-    ctx.set(bus.awvalid, 0)
-    ctx.set(bus.wvalid, 0)
-
-async def axi4lite_read(ctx, bus, addr):
-    ctx.set(bus.rready, 1)
-    ctx.set(bus.arvalid, 1)
-    ctx.set(bus.araddr, addr)
-    await ctx.tick()
-    return ctx.get(bus.rdata)
-
 def matmul_case(m, k, n, config):
     np.set_printoptions(linewidth=150)
     a_dtype, b_dtype, c_dtype = config.act_dtype, config.weight_dtype, config.acc_dtype
     a = np.random.randint(*(a_bounds := dtype_to_bounds(a_dtype)), size=(m, k))
     b = np.random.randint(*dtype_to_bounds(b_dtype), size=(k, n))
-    # print("a:")
-    # print(a)
-    # for row in a:
-    #     print(hex(packed(row.tolist(), config.act_dtype.width))[2:])
-    # print("b:")
-    # print(b)
-    # for row in b:
-    #     print(hex(packed(row.tolist(), config.weight_dtype.width))[2:])
-    # print("psums:")
-    # for i in range(2):
-    #     print(f"psum {i}:")
-    #     print(a[:, i * 8:(i + 1) * 8] @ b[i*8:(i + 1)*8, :])
 
     #NOTE: if c values are too large, then a and b psums become irrelevant after saturating scaling
     testc_dtype = IntType(width=a_dtype.width + b_dtype.width, signed=a_dtype.signed or b_dtype.signed)
     c = np.random.randint(*dtype_to_bounds(testc_dtype), size=(1, n))
 
-    # print(c)
-    # print("c:", hex(packed(c[0].tolist(), config.acc_dtype.width))[2:])
-
     actfn = random.choice(list(Activation))
     zd    = random.randint(*a_bounds)
-    shamt = np.random.randint(30, 40, size=(1,))
+    shamt = np.random.randint(30, c_dtype.width * 2 - 1, size=(1,))
     qmul  = np.random.randint(0, dtype_to_bounds(c_dtype)[1], size=(1,))
     expected = qmatmul(a, b, c, zd, qmul, shamt, a_dtype, c_dtype, actfn=actfn)
-
-    # print("exp:")
-    # print(expected)
-    # for row in expected:
-    #     print(hex(packed(row.tolist(), config.act_dtype.width))[2:])
-
-    # a_words, b_words = reshape_for_matmul(a, b, config)
-    # a_bytes = pack_activations(a, config)
-    # b_bytes = pack_weights(b, config)
-    # c_bytes = pack_bias(c, config)
-    # nblocks = -(-n // config.cols)
-    # pexp = np.pad(expected, ((0, 0), (0, nblocks * config.cols - n))).reshape(m, nblocks, config.cols).transpose(1, 0, 2).reshape(-1, config.cols)
-    # expected_words = [word for row in pexp.tolist() for word in repack(row, config.act_dtype.width, config.host_data_width)]
     return pack_activations(a, config), pack_weights(b, config), pack_bias(c, config), actfn, zd, shamt, qmul, expected
 
 def batched(n, m):
