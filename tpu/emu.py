e9da1593b79de643da98b41c4b17f8455f745b7e
Sohaib Errabii <errabii.sohaib@gmail.com>
2025-11-09 06:34:20 +0100

    tpu.py is back, test_emu.py

--- tpu/emu.py
+++ tpu/emu.py
@@ -1,10 +1,9 @@
 import numpy as np
 
-from core import TPUConfig
-from isa import *
-from sw import *
+from tpu.tpu import TPUConfig
+from tpu.isa import *
+from tpu.sw import *
 
-from test.helpers import matmul_case
 
 def pack(dat: np.ndarray, dtype: IntType):
     return np.asarray([signed_unpack(packed(row.tolist(), 8), dat.shape[1] * 8, dtype) for row in dat], dtype=dtype.numpy)
@@ -31,9 +30,6 @@ def run(instrs, config: TPUConfig, mem):
 
     for instr in instrs:
         match instr["op"]:
-            case Op.NOP:
-                pass
-
             case Op.SCALER_CONFIG:
                 qmul, shamt, zp = [instr["args"]["config"][k] for k in ["qmul", "shamt", "zp"]]
 
@@ -94,37 +90,5 @@ def run(instrs, config: TPUConfig, mem):
                 laddr = instr["args"]["exec"]["addr2"]["act"]
                 mem[haddr:haddr + reps * act_mem.shape[1]] = act_mem[laddr:laddr + reps].reshape(-1)
 
-
-if __name__ == '__main__':
-    config = TPUConfig()
-    mem = np.zeros((4 * 1024 * 1024), dtype=np.uint8)
-
-    m, k, n = 8, 8, 8
-    a_bytes, b_bytes, c_bytes, actfn, zp, shamt, qmul, expected = matmul_case(m, k, n, config)
-
-    mem[:len(b_bytes)] = b_bytes
-    mem[len(b_bytes):len(b_bytes) + len(a_bytes)] = a_bytes
-
-    c_offset = len(a_bytes) + len(b_bytes)
-    mem[c_offset:c_offset + len(c_bytes)] = c_bytes
-    d_offset = c_offset + len(c_bytes)
-
-    instrs = [
-        scaler_config(qmul, shamt, zp),
-        load_hb(c_offset, 31, 1),
-        load_ha(len(b_bytes), 0, 8),
-        load_hw(0, 8),
-        load_w(wsel=0),
-        # acc_sync(),
-        # preload_sync(),
-        # spad_sync(),
-        matmul(0, 0, 31, 8, wsel=0, acc=AccMode.CONST),
-        # acc_sync(),
-        activate(0, 0, 8, actfn=actfn),
-        # spad_sync(),
-        store(d_offset, 0, 8),
-        # spad_sync(),
-    ]
-    run(instrs, config, mem)
-    result = unpack_activations(mem[d_offset:d_offset + m * config.act_dtype.width * config.rows // 8], m, n, config)
-    np.testing.assert_array_equal(result, expected)
+            case _:
+                pass
